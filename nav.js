e03c24a22cf3642c888ffdb4667b79ecd0825665
Allen Manoj <allenmanoj17@gmail.com>
2021-07-22 00:21:57 +0530

    commit

--- nav.js
+++ nav.js
@@ -31,3 +31,18 @@ $(".form__btn").click(function () {
     });
   });
   
+
+  
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
